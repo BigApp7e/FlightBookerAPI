63b7907ba04fdd0d88b608852afebbc297eac5fa
bigapp7e <bigapp7e@gmail.com>
2025-06-29 16:33:20 +0300

    set log

--- src/main/java/bsu/controllers/FlightController.java
+++ src/main/java/bsu/controllers/FlightController.java
@@ -5,12 +5,14 @@ import bsu.dto.FlySearchRequest;
 import bsu.dto.FlySearchResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -20,6 +22,9 @@ public class FlightController {
 
     @PostMapping("/search")
     public ResponseEntity<List<FlySearchResponse>> handlePostRequest(@Valid @RequestBody FlySearchRequest request) {
+
+        log.info("Floght Controller Call");
+
         List<FlySearchResponse> response = flightService.getFlyight(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
