5bfdd0b03ec4e2e107704be814db407c5f7b46be
bigapp7e <bigapp7e@gmail.com>
2025-06-29 16:49:40 +0300

    set log

--- src/main/java/bsu/controllers/FlightController.java
+++ src/main/java/bsu/controllers/FlightController.java
@@ -23,7 +23,7 @@ public class FlightController {
     @PostMapping("/search")
     public ResponseEntity<List<FlySearchResponse>> handlePostRequest(@Valid @RequestBody FlySearchRequest request) {
 
-        log.info("Floght Controller Call");
+        log.info("Floght Controller ");
 
         List<FlySearchResponse> response = flightService.getFlyight(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
